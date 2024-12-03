288f434700b272f23aeac5951a2553c58000babb
Zeptiny <arturtrentinibenedetti@gmail.com>
2024-12-03 16:34:38 -0300

    We got dark mode! But only on index

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,11 @@ module.exports = {
     extend: {
       colors: {
         'dark-surface':'#0F172A',//Background - slate-900
-        'dark-onSurface':'#D1D1D1',//Text on Background
-        'dark-surfaceContainerLow':'#334155',//Container on background
+        'dark-onLow':'#899ec2',//Text on Background
+        'dark-onSurface':'#c4c4c4',//Text on Background
+        'dark-surfaceContainerLow':'#27354c',//Container on background
         'dark-surfaceContainer':'#1E293B',//Container on background - slate-800
-        'dark-surfaceContainerHigh':'#D1D1D1',//Container on background
+        'dark-surfaceContainerHigh':'#151d2a',//Container on background
         'dark-primary':'#ffe3f4',//Primary Color
         'dark-onPrimary':'#F2F2F2',//Text on Primary Color
         'dark-secondary':'#fff0f5',//Yada Yada...
